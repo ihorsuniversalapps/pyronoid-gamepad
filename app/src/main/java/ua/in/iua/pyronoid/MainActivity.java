e8e0203a64a83f1184224b170633895b45e4866a
ihor rusin <ihor.rusin@onlini.co>
2016-08-09 10:43:17 +0300

    Now, the game is working!

--- app/src/main/java/ua/in/iua/pyronoid/MainActivity.java
+++ app/src/main/java/ua/in/iua/pyronoid/MainActivity.java
@@ -3,6 +3,9 @@ package ua.in.iua.pyronoid;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 import net.razorvine.pyro.NameServerProxy;
 import net.razorvine.pyro.PyroProxy;
@@ -11,40 +14,71 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "Pyronoid";
+    private volatile double curPosX = 0;
+    private View moveView;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            curPosX = event.getX();
+            return true;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Thread(new Runnable() {
+        moveView = findViewById(R.id.vTouchView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        moveView.post(new Runnable() {
             @Override
             public void run() {
-                NameServerProxy ns = null;
-                PyroProxy batMover = null;
-                try {
-                    ns = NameServerProxy.locateNS(null);
+                final long maxX = moveView.getMeasuredWidth();
 
-                    batMover = new PyroProxy(ns.lookup("PYRONAME:local.pyronoid"));
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        NameServerProxy ns = null;
+                        PyroProxy batMover = null;
+                        try {
+                            ns = NameServerProxy.locateNS(null);
 
-                    while (true) {
-                        Log.d("666", "run: Sent");
-                        batMover.call("hello", 42);
-                        Thread.sleep(1000);
+                            batMover = new PyroProxy(ns.lookup("PYRONAME:local.pyronoid"));
+
+                            while (true) {
+                                double newX = curPosX / (double) maxX;
+                                Log.d(TAG, String.format("run: newX: %1.5f", newX));
+                                batMover.call("move", newX);
+                                Thread.sleep(10);
+                            }
+
+
+                        } catch (IOException | InterruptedException e) {
+                            e.printStackTrace();
+                        } finally {
+                            Toast.makeText(MainActivity.this, "Connection is closed", Toast.LENGTH_SHORT).show();
+
+                            if (batMover != null) {
+                                batMover.close();
+                            }
+                            if (ns != null) {
+                                ns.close();
+                            }
+                        }
                     }
-
-
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-                finally {
-                    if (batMover != null) {
-                        batMover.close();
-                    }
-                    if (ns != null) {
-                        ns.close();
-                    }
-                }
+                }).start();
             }
-        }).start();
+        });
     }
+
 }
